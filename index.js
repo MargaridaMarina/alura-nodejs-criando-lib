bd6377f380e88b23e1f30be999f33a31ceef007a
Margarida <margaridammcm@gmail.com>
2022-07-01 17:29:55 -0300

    Syntax async await

--- index.js
+++ index.js
@@ -5,22 +5,15 @@ function trataErro(erro){
   throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'))
 }
 
-function pegaArquivo(caminhoDoArquivo){
-  const encoding = 'utf-8'
-  fs.promises
-  .readFile(caminhoDoArquivo, encoding)
-  .then((texto) => chalk.green(console.log(texto)))
-  .catch((erro) => trataErro(erro))
-}
 
-// function pegaArquivo(caminhoDoArquivo){
-//   const encoding = 'utf-8'
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
-//     if(erro){
-//       trataErro(erro)
-//     }
-//     console.log(chalk.green(texto))
-//   })
-// }
+async function pegaArquivo(caminhoDoArquivo){
+  try {
+    const encoding = 'utf-8'
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto))
+  } catch(erro) {
+    trataErro(erro)
+  }
+}
 
 pegaArquivo('./arquivos/')
