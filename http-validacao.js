6441c7f3a928aa6c0d37a6bdc4e854fdf5a16fae
Margarida <margaridammcm@gmail.com>
2022-07-04 10:55:45 -0300

    Remontando objeto, acrescentando status

--- http-validacao.js
+++ http-validacao.js
@@ -1,21 +1,28 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 async function checaStatus(arrayURLs) {
-  const arrayStatus = await Promise.all(arrayURLs.map(async url => {
-    const res = await fetch(url)
-    return res.status
+  const arrayStatus = await Promise
+    .all(arrayURLs
+      .map(async url => {
+        const res = await fetch(url)
+        return res.status
   }))
   return arrayStatus
 }
 
 function geraArrayDeURLs(arrayLinks){
-  return arrayLinks.map(objetoLink => Object.values(objetoLink).join())
+  return arrayLinks
+    .map(objetoLink => Object
+      .values(objetoLink).join())
 }
 
 async function validaURLs(arrayLinks) {
   const links = geraArrayDeURLs(arrayLinks)
   const statusLinks = await checaStatus(links)
-  return statusLinks
+  const resultados = arrayLinks.map((objeto, indice) => ({
+    ...objeto, 
+    status: statusLinks[indice]}))
+  return resultados
 }
 
 module.exports = validaURLs
