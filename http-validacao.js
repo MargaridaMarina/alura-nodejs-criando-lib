d790069cacb0747034e2ba29cb50ffd5f5907867
Margarida <margaridammcm@gmail.com>
2022-07-04 10:47:50 -0300

    Trazendo status das requisicoes de cada link

--- http-validacao.js
+++ http-validacao.js
@@ -1,9 +1,21 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+async function checaStatus(arrayURLs) {
+  const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+    const res = await fetch(url)
+    return res.status
+  }))
+  return arrayStatus
+}
+
 function geraArrayDeURLs(arrayLinks){
   return arrayLinks.map(objetoLink => Object.values(objetoLink).join())
 }
 
-function validaURLs(arrayLinks) {
-  return geraArrayDeURLs(arrayLinks)
+async function validaURLs(arrayLinks) {
+  const links = geraArrayDeURLs(arrayLinks)
+  const statusLinks = await checaStatus(links)
+  return statusLinks
 }
 
 module.exports = validaURLs
