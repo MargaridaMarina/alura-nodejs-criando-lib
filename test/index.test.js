024bcc7ee5a148288701f30e5604a2c1f9f73403
Margarida <margaridammcm@gmail.com>
2022-07-04 15:05:00 -0300

    Criando teste

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,18 @@
 const pegaArquivo = require('../index')
 
-test ('deve ser uma funcao', () => {
-  expect(typeof pegaArquivo).toBe('function')
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+]
+
+describe('pegaArquivo::', () => {
+  it('deve ser uma funcao', () => {
+    expect(typeof pegaArquivo).toBe('function')
+  })
+  it('deve retornar array com resultados', async () => {
+    const resultado = await pegaArquivo('/home/margo/code/alura-nodejs-criando-lib/test/arquivos/texto1.md')
+    expect(resultado).toEqual(arrayResult)
+  })
 })
 
